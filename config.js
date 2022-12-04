1a49d3b0c71dd77128337e2107e9728a211abe6d
cqxflyx <119166034+cqxflyx@users.noreply.github.com>
2022-12-04 17:06:08 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,34 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'Ariasakaの监控',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm793121182-6a8c95987ed329cef6a95e60',
+'m793182900-8b325a6860f19a7cb92ecc27',
+  ],
+
+  // 日志天数
+  CountDays: 30,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://ariasaka.top'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/lyxofficial'
+    },
+    {
+      text: 'Blog',
+      url: 'https://yisous.xyz/'
+    },
+  ],
+};
